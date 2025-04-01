790c12968f75d46308957faa2c7addc4c3c7c668
thrly <22343302+thrly@users.noreply.github.com>
2025-04-01 14:03:49 +0100

    PCB version 0.01

--- README.md
+++ README.md
@@ -1,15 +1,32 @@
 # TEMPEST
 
-a 36-key split ergonomic wireless keyboard.
+_a 36-key split ergonomic wireless keyboard._
 
-![Tempest Image]()
+![Tempest Image](/tempest-pcb-v01.png)
+
+> [!CAUTION]
+> v.0.1 : Currently awaiting PCB to tests. There are some potential collisions on the board design which may cause issues.
 
 ## Design
 
 - Five columns, three rows, three thumb keys.
-- Column stagger and splay on the pinky column.
-- Powered by nice!nano microcontroller (and optional nice!view display)
+- Column stagger and splay on the pinky and ring columns.
+- Powered by nice!nano microcontroller / pro micro (and optional nice!view display).
 - Low profile Choc switches.
+- Reversible PCB.
+- Uses some components from the Typeractive Corne design.
+- Layout with Ergogen (see [config.yaml](./ergogen/config.yaml)).
+
+## Using this repo
+
+The ergogen files are contained in the ergogen/ folder.
+
+Gerber files for PCB fabrication (i.e. JLPCB) can be found in /tempest-gerbers/ for ordering.
+
+Latest KiCad PCB is included, but files can be generated using Ergogen.
+
+> [!IMPORTANT]
+> Some of the ergogen/ceoloide footprint files have been modified slightly from the [originals](https://github.com/ceoloide/ergogen-footprints).
 
 ## Influences
 
--- ergogen/footprints/ceoloide/power_switch_smd_side.js
+++ ergogen/footprints/ceoloide/power_switch_smd_side.js
@@ -1,3 +1,5 @@
+// thrly: edited power labels to +/-
+
 // Copyright (c) 2023 Marco Massarelli
 //
 // SPDX-License-Identifier: CC-BY-NC-SA-4.0
@@ -60,20 +62,20 @@
 
 module.exports = {
   params: {
-    designator: 'PWR',
-    side: 'F',
+    designator: "PWR",
+    side: "F",
     reversible: false,
     invert_behavior: true,
     include_silkscreen: true,
     include_courtyard: false,
-    switch_3dmodel_filename: '',
+    switch_3dmodel_filename: "",
     switch_3dmodel_xyz_offset: [0, 0, 0],
     switch_3dmodel_xyz_rotation: [0, 0, 0],
     switch_3dmodel_xyz_scale: [1, 1, 1],
-    from: { type: 'net', value: 'BAT_P' },
-    to: { type: 'net', value: 'RAW' },
+    from: { type: "net", value: "BAT_P" },
+    to: { type: "net", value: "RAW" },
   },
-  body: p => {
+  body: (p) => {
     const common_start = `
   (footprint "ceoloide:power_switch_smd_side"
     (layer "${p.side}.Cu")
@@ -85,12 +87,16 @@ module.exports = {
       (effects (font (size 1 1) (thickness 0.15)))
     )
     (attr smd)
-    `
+    `;
     const silkscreen_front = `
-    (fp_text user "ON" (at 0 ${p.invert_behavior ? '-' : ''}5 ${p.r}) (layer "F.SilkS")
+    (fp_text user "+" (at 0 ${p.invert_behavior ? "-" : ""}4.2 ${
+      p.r
+    }) (layer "F.SilkS")
       (effects (font (size 1 1) (thickness 0.15)))
     )
-    (fp_text user "OFF" (at 0 ${p.invert_behavior ? '' : '-'}5 ${p.r}) (layer "F.SilkS")
+    (fp_text user "-" (at 0 ${p.invert_behavior ? "" : "-"}4.2 ${
+      p.r
+    }) (layer "F.SilkS")
       (effects (font (size 1 1) (thickness 0.15)))
     )
     (fp_line (start 0.415 -3.45) (end -0.375 -3.45) (layer "F.SilkS") (stroke (width 0.12) (type solid)))
@@ -98,15 +104,21 @@ module.exports = {
     (fp_line (start -1.425 1.6) (end -1.425 -0.1) (layer "F.SilkS") (stroke (width 0.12) (type solid)))
     (fp_line (start 1.425 2.85) (end 1.425 -2.85) (layer "F.SilkS") (stroke (width 0.12) (type solid)))
     (fp_line (start -1.425 -1.4) (end -1.425 -1.6) (layer "F.SilkS") (stroke (width 0.12) (type solid)))
-    `
+    `;
     const silkscreen_back = `
-    (fp_text user "${p.ref}" (at -3.5 0 ${90 + p.r}) (layer "B.SilkS") ${p.ref_hide}
+    (fp_text user "${p.ref}" (at -3.5 0 ${90 + p.r}) (layer "B.SilkS") ${
+      p.ref_hide
+    }
       (effects (font (size 1 1) (thickness 0.15)) (justify mirror))
     )
-    (fp_text user "ON" (at 0 ${p.invert_behavior ? '-' : ''}5 ${p.r}) (layer "B.SilkS")
+    (fp_text user "+" (at 0 ${p.invert_behavior ? "-" : ""}4.2 ${
+      p.r
+    }) (layer "B.SilkS")
       (effects (font (size 1 1) (thickness 0.15)) (justify mirror))
     )
-    (fp_text user "OFF" (at 0 ${p.invert_behavior ? '' : '-'}5 ${p.r}) (layer "B.SilkS")
+    (fp_text user "-" (at 0 ${p.invert_behavior ? "" : "-"}4.2 ${
+      p.r
+    }) (layer "B.SilkS")
       (effects (font (size 1 1) (thickness 0.15)) (justify mirror))
     )
     (fp_line (start -1.425 1.4) (end -1.425 1.6) (layer "B.SilkS") (stroke (width 0.12) (type solid)))
@@ -114,7 +126,7 @@ module.exports = {
     (fp_line (start -0.375 -3.45) (end 0.415 -3.45) (layer "B.SilkS") (stroke (width 0.12) (type solid)))
     (fp_line (start -1.425 -1.6) (end -1.425 0.1) (layer "B.SilkS") (stroke (width 0.12) (type solid)))
     (fp_line (start 1.425 -2.85) (end 1.425 2.85) (layer "B.SilkS") (stroke (width 0.12) (type solid)))
-    `
+    `;
     const courtyard_front = `
     (fp_line (start 1.795 4.4) (end -2.755 4.4) (layer "F.CrtYd") (stroke (width 0.05) (type solid)))
     (fp_line (start 1.795 1.65) (end 1.795 4.4) (layer "F.CrtYd") (stroke (width 0.05) (type solid)))
@@ -124,7 +136,7 @@ module.exports = {
     (fp_line (start 1.795 -4.4) (end 1.795 -1.65) (layer "F.CrtYd") (stroke (width 0.05) (type solid)))
     (fp_line (start -2.755 -4.4) (end 1.795 -4.4) (layer "F.CrtYd") (stroke (width 0.05) (type solid)))
     (fp_line (start -2.755 4.4) (end -2.755 -4.4) (layer "F.CrtYd") (stroke (width 0.05) (type solid)))
-    `
+    `;
     const courtyard_back = `
     (fp_line (start -2.755 -4.4) (end -2.755 4.4) (layer "B.CrtYd") (stroke (width 0.05) (type solid)))
     (fp_line (start 3.095 1.65) (end 3.095 -1.65) (layer "B.CrtYd") (stroke (width 0.05) (type solid)))
@@ -134,7 +146,7 @@ module.exports = {
     (fp_line (start 3.095 -1.65) (end 1.795 -1.65) (layer "B.CrtYd") (stroke (width 0.05) (type solid)))
     (fp_line (start 1.795 4.4) (end 1.795 1.65) (layer "B.CrtYd") (stroke (width 0.05) (type solid)))
     (fp_line (start -2.755 4.4) (end 1.795 4.4) (layer "B.CrtYd") (stroke (width 0.05) (type solid)))
-    `
+    `;
 
     const pads_front = `
     (fp_line (start -1.305 -3.35) (end -1.305 3.35) (layer "F.Fab") (stroke (width 0.1) (type solid)))
@@ -147,14 +159,32 @@ module.exports = {
     (fp_line (start 2.845 1.2) (end 2.845 0.35) (layer "F.Fab") (stroke (width 0.1) (type solid)))
     (fp_line (start 2.645 1.4) (end 2.845 1.2) (layer "F.Fab") (stroke (width 0.1) (type solid)))
     (fp_line (start 1.345 1.4) (end 2.645 1.4) (layer "F.Fab") (stroke (width 0.1) (type solid)))
-    (pad "" smd rect (at 1.125 -3.65 ${90 + p.r}) (size 1 0.8) (layers "F.Cu" "F.Paste" "F.Mask"))
-    (pad "" smd rect (at -1.085 -3.65 ${90 + p.r}) (size 1 0.8) (layers "F.Cu" "F.Paste" "F.Mask"))
-    (pad "" smd rect (at -1.085 3.65 ${90 + p.r}) (size 1 0.8) (layers "F.Cu" "F.Paste" "F.Mask"))
-    (pad "" smd rect (at 1.125 3.65 ${90 + p.r}) (size 1 0.8) (layers "F.Cu" "F.Paste" "F.Mask"))
-    (pad "1" smd rect (at -1.735 2.25 ${90 + p.r}) (size 0.7 1.5) (layers "F.Cu" "F.Paste" "F.Mask") ${p.invert_behavior ? '' : p.from.str})
-    (pad "2" smd rect (at -1.735 -0.75 ${90 + p.r}) (size 0.7 1.5) (layers "F.Cu" "F.Paste" "F.Mask") ${p.to.str})
-    (pad "3" smd rect (at -1.735 -2.25 ${90 + p.r}) (size 0.7 1.5) (layers "F.Cu" "F.Paste" "F.Mask") ${p.invert_behavior ? p.from.str : ''})
-    `
+    (pad "" smd rect (at 1.125 -3.65 ${
+      90 + p.r
+    }) (size 1 0.8) (layers "F.Cu" "F.Paste" "F.Mask"))
+    (pad "" smd rect (at -1.085 -3.65 ${
+      90 + p.r
+    }) (size 1 0.8) (layers "F.Cu" "F.Paste" "F.Mask"))
+    (pad "" smd rect (at -1.085 3.65 ${
+      90 + p.r
+    }) (size 1 0.8) (layers "F.Cu" "F.Paste" "F.Mask"))
+    (pad "" smd rect (at 1.125 3.65 ${
+      90 + p.r
+    }) (size 1 0.8) (layers "F.Cu" "F.Paste" "F.Mask"))
+    (pad "1" smd rect (at -1.735 2.25 ${
+      90 + p.r
+    }) (size 0.7 1.5) (layers "F.Cu" "F.Paste" "F.Mask") ${
+      p.invert_behavior ? "" : p.from.str
+    })
+    (pad "2" smd rect (at -1.735 -0.75 ${
+      90 + p.r
+    }) (size 0.7 1.5) (layers "F.Cu" "F.Paste" "F.Mask") ${p.to.str})
+    (pad "3" smd rect (at -1.735 -2.25 ${
+      90 + p.r
+    }) (size 0.7 1.5) (layers "F.Cu" "F.Paste" "F.Mask") ${
+      p.invert_behavior ? p.from.str : ""
+    })
+    `;
     const pads_back = `
     (fp_line (start 2.595 -0.1) (end 1.295 -0.1) (layer "B.Fab") (stroke (width 0.1) (type solid)))
     (fp_line (start -1.305 3.35) (end -1.305 -3.35) (layer "B.Fab") (stroke (width 0.1) (type solid)))
@@ -166,19 +196,41 @@ module.exports = {
     (fp_line (start 1.295 -3.35) (end 1.295 3.35) (layer "B.Fab") (stroke (width 0.1) (type solid)))
     (fp_line (start 1.295 3.35) (end -1.305 3.35) (layer "B.Fab") (stroke (width 0.1) (type solid)))
     (fp_line (start -1.305 -3.35) (end 1.295 -3.35) (layer "B.Fab") (stroke (width 0.1) (type solid)))
-    (pad "" smd rect (at -1.085 -3.65 ${270 + p.r}) (size 1 0.8) (layers "B.Cu" "B.Paste" "B.Mask"))
-    (pad "" smd rect (at 1.125 -3.65 ${270 + p.r}) (size 1 0.8) (layers "B.Cu" "B.Paste" "B.Mask"))
-    (pad "" smd rect (at -1.085 3.65 ${270 + p.r}) (size 1 0.8) (layers "B.Cu" "B.Paste" "B.Mask"))
-    (pad "" smd rect (at 1.125 3.65 ${270 + p.r}) (size 1 0.8) (layers "B.Cu" "B.Paste" "B.Mask"))
-    (pad "1" smd rect (at -1.735 -2.25 ${270 + p.r}) (size 0.7 1.5) (layers "B.Cu" "B.Paste" "B.Mask") ${p.invert_behavior ? p.from.str : ''})
-    (pad "2" smd rect (at -1.735 0.75 ${270 + p.r}) (size 0.7 1.5) (layers "B.Cu" "B.Paste" "B.Mask") ${p.to.str})
-    (pad "3" smd rect (at -1.735 2.25 ${270 + p.r}) (size 0.7 1.5) (layers "B.Cu" "B.Paste" "B.Mask") ${p.invert_behavior ? '' : p.from.str})
-    `
+    (pad "" smd rect (at -1.085 -3.65 ${
+      270 + p.r
+    }) (size 1 0.8) (layers "B.Cu" "B.Paste" "B.Mask"))
+    (pad "" smd rect (at 1.125 -3.65 ${
+      270 + p.r
+    }) (size 1 0.8) (layers "B.Cu" "B.Paste" "B.Mask"))
+    (pad "" smd rect (at -1.085 3.65 ${
+      270 + p.r
+    }) (size 1 0.8) (layers "B.Cu" "B.Paste" "B.Mask"))
+    (pad "" smd rect (at 1.125 3.65 ${
+      270 + p.r
+    }) (size 1 0.8) (layers "B.Cu" "B.Paste" "B.Mask"))
+    (pad "1" smd rect (at -1.735 -2.25 ${
+      270 + p.r
+    }) (size 0.7 1.5) (layers "B.Cu" "B.Paste" "B.Mask") ${
+      p.invert_behavior ? p.from.str : ""
+    })
+    (pad "2" smd rect (at -1.735 0.75 ${
+      270 + p.r
+    }) (size 0.7 1.5) (layers "B.Cu" "B.Paste" "B.Mask") ${p.to.str})
+    (pad "3" smd rect (at -1.735 2.25 ${
+      270 + p.r
+    }) (size 0.7 1.5) (layers "B.Cu" "B.Paste" "B.Mask") ${
+      p.invert_behavior ? "" : p.from.str
+    })
+    `;
     const common_end = `
-    (pad "" np_thru_hole circle (at 0.025 -1.5 ${90 + p.r}) (size 0.9 0.9) (drill 0.9) (layers "*.Cu" "*.Mask"))
-    (pad "" np_thru_hole circle (at 0.025 1.5 ${90 + p.r}) (size 0.9 0.9) (drill 0.9) (layers "*.Cu" "*.Mask"))
+    (pad "" np_thru_hole circle (at 0.025 -1.5 ${
+      90 + p.r
+    }) (size 0.9 0.9) (drill 0.9) (layers "*.Cu" "*.Mask"))
+    (pad "" np_thru_hole circle (at 0.025 1.5 ${
+      90 + p.r
+    }) (size 0.9 0.9) (drill 0.9) (layers "*.Cu" "*.Mask"))
   )
-    `
+    `;
 
     const switch_3dmodel = `
     (model ${p.switch_3dmodel_filename}
@@ -186,33 +238,33 @@ module.exports = {
       (scale (xyz ${p.switch_3dmodel_xyz_scale[0]} ${p.switch_3dmodel_xyz_scale[1]} ${p.switch_3dmodel_xyz_scale[2]}))
       (rotate (xyz ${p.switch_3dmodel_xyz_rotation[0]} ${p.switch_3dmodel_xyz_rotation[1]} ${p.switch_3dmodel_xyz_rotation[2]}))
     )
-    `
+    `;
 
     let final = common_start;
     if (p.side == "F" || p.reversible) {
-      final += pads_front
+      final += pads_front;
       if (p.include_silkscreen) {
-        final += silkscreen_front
+        final += silkscreen_front;
       }
       if (p.include_courtyard) {
-        final += courtyard_front
+        final += courtyard_front;
       }
     }
     if (p.side == "B" || p.reversible) {
-      final += pads_back
+      final += pads_back;
       if (p.include_silkscreen) {
-        final += silkscreen_back
+        final += silkscreen_back;
       }
       if (p.include_courtyard) {
-        final += courtyard_back
+        final += courtyard_back;
       }
     }
 
     if (p.switch_3dmodel_filename) {
-      final += switch_3dmodel
+      final += switch_3dmodel;
     }
 
     final += common_end;
     return final;
-  }
-}
+  },
+};
